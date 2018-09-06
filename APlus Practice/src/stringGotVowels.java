fe92a56f5656d6180d6be28d071239bc72d3eb99
elena.oman <elena.oman@MU-C1-21.mullen.pvt.k12.co.us>
2018-09-06 14:59:51 -0600

    APlus First Nine

---
+++ APlus Practice/src/stringGotVowels.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class stringGotVowels
+	{
+		Scanner text = new Scanner(System.in);
+		String userText = text.nextLine();
+
+		for(int i = 0; i < userText.length(); i++)
+			{
+				
+			
+				switch(userText)
+					{
+						case "a":
+						case "A":
+						case "e":
+						case "E":
+						case "i":
+						case "I":
+						case "o":
+						case "O":
+						case "u":
+						case "U": 
+							break;
+					}
+					default
+					{
+					return yes;
+					}
+				
+
+			}
+			}
+
+	}
